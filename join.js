59b45e480a3cc91ec4f71e8efd20ba59abc0ea17
rtm516 <ryantmilner@hotmail.co.uk>
2021-02-09 14:17:42 +0000

    Move communities.json to automatic generation

---
+++ join.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const discordCommunities = {}
+discordCommunities.data = []
+
+// Load all community json files from the communities directory
+fs.readdirSync('./communities/').forEach(communityFile => {
+  const community = require('./communities/' + communityFile)
+  discordCommunities.data.push(community)
+})
+
+// Write the new communities.json file
+fs.writeFileSync(`./communities.json`, JSON.stringify(discordCommunities, null, 2))
